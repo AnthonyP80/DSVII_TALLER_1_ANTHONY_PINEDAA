98662a1a9bd91bd5266ca5d28a7ecacbeaa8d81f
AnthonyP80 <anthonii.p08@gmail.com>
2025-10-13 16:38:52 -0500

    TALLER_7 - Paso 3: Implementación de sistema de login simple

--- TALLERES/TALLER_7/usar_sesion.php
+++ TALLERES/TALLER_7/usar_sesion.php
@@ -2,7 +2,7 @@
 session_start();
 
 if(isset($_SESSION['usuario'])) {
-    echo "Bienvenida, " . htmlspecialchars($_SESSION['usuario']) . "!<br>";
+    echo "Bienvenido, " . htmlspecialchars($_SESSION['usuario']) . "!<br>";
     echo "Tu rol es: " . htmlspecialchars($_SESSION['rol']);
 } else {
     echo "No has iniciado sesión.";
